205c3569697dcc0f7a9b3b004a8f51c726104f7e
KimberlyZhan <112623368+KimberlyZhan@users.noreply.github.com>
2022-10-03 15:33:23 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,16 @@
 // JavaScript Document
 console.log("hi");
 
-var achtergrondMuziek = document.querySelector("#achtergrond-geluid");
+//------------------- voor de achtergrond muziek -------------------//
+var achtergrondMuziek = new Audio("audio/cooking_mama_bg_music.mp3");
 var geluidIcoon = document.querySelector(".wel-geluid-icoon");
 var muziekStatus = "aan";
-
-// function muziekjeAan() {
-//     geluidIcoon.src = "images/geenGeluid.png";
-//     muziekStatus = "uit";
-
-//     achtergrondMuziek.pause();
-// }
 
 function muziekjeAan() {
     if (muziekStatus == "uit") {
         geluidIcoon.src = "images/welGeluid.png";
         achtergrondMuziek.play();
-        muziekStatus = "aan"
+        muziekStatus = "aan";
     } else {
         geluidIcoon.src = "images/geenGeluid.png";
         achtergrondMuziek.pause();
@@ -26,3 +20,66 @@ function muziekjeAan() {
 
 geluidIcoon.addEventListener("click", muziekjeAan);
 
+//------------------------ voor kerst thema ------------------------//
+var kerstCanvas = document.querySelector(".kerst-thema-canvas");
+var kerstBalButton = document.querySelector(".kerst-bal-icoon");
+var geenKerstButton = document.querySelector(".geen-kerst-icoon");
+var sneeuwBG = document.querySelector(".sneeuw-bg");
+var kerstman = document.querySelector(".kerstman-met-slee");
+var kerstSter = document.querySelector(".kerst-ster");
+var kerstHoek = document.querySelector(".kerst-hoekje");
+
+var kerstMuziek = new Audio("audio/kerst_muziek.mp3");
+
+var kerstThema = "aan";
+
+
+function kerst() {
+    if (kerstThema == "uit") {
+        kerstBalButton.src = "images/kerst_bal.png";
+        kerstMuziek.play(); 
+        kerstThema = "aan";
+
+        sneeuwBG.hidden = false;
+        kerstman.hidden = false;
+        kerstSter.hidden = false;
+        kerstHoek.hidden = false;
+    } else {
+        kerstBalButton.src = "images/no_kerst.png";
+        kerstMuziek.pause();
+        kerstThema = "uit";
+
+        sneeuwBG.hidden = true;
+        kerstman.hidden = true;
+        kerstSter.hidden = true;
+        kerstHoek.hidden = true;
+    }
+}
+
+kerstBalButton.addEventListener("click", kerst);
+
+// function kerstAan() {
+//     kerstThema = "aan";
+//     kerstBalButton.src = "images/kerst_bal.png";
+//     kerstMuziek.play();
+
+//     sneeuwBG.hidden = false;
+//     kerstman.hidden = false;
+//     kerstSter.hidden = false;
+//     kerstHoek.hidden = false;
+// }
+
+// kerstBalButton.addEventListener("click", kerstAan);
+
+// function kerstUit() {
+//     kerstThema = "uit";
+//     kerstBalButton.src = "images/no_kerst.png";
+//     kerstMuziek.pause();
+
+//     sneeuwBG.hidden = true;
+//     kerstman.hidden = true;
+//     kerstSter.hidden = true;
+//     kerstHoek.hidden = true;
+// }
+
+// geenKerstButton.addEventListener("click", kerstUit);
